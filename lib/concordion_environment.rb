98216d3f5dede430f78a95bdfe7d6111c25424fd
b.goodspeed <b.goodspeed@5e4cd714-1f4c-0410-be64-ad5e8ed6b23e>
2008-12-08 14:07:06 +0000

    fixed the non-rcor specific variable name, and dried up references to it

--- lib/concordion_environment.rb
+++ lib/concordion_environment.rb
@@ -1,11 +1,15 @@
 
 class ConcordionEnvironment
+  def self.output_dir_key
+    'RCOR_OUTPUT_DIR'
+  end
+  
   def self.clean_list
     File.join(output_dir, "*_test_output.html")
   end
 
   def self.output_dir
-    dir = ENV['OUTPUT_DIR'] 
+    dir = ENV[output_dir_key] 
     dir.nil? ? "." : dir
   end
 end
--- tests/concordion_environment_test.rb
+++ tests/concordion_environment_test.rb
@@ -2,29 +2,29 @@ require 'test_helper'
 require 'concordion_environment'
 
 class ConcordionEnvironmentTest < Test::Unit::TestCase
-
+  
   def test_output_dir
     restore_env do
-      ENV['OUTPUT_DIR'] = nil
+      ENV[ConcordionEnvironment.output_dir_key] = nil
       assert_equal ".", ConcordionEnvironment.output_dir
-      ENV['OUTPUT_DIR'] = "foo/bar"
+      ENV[ConcordionEnvironment.output_dir_key] = "foo/bar"
       assert_equal "foo/bar", ConcordionEnvironment.output_dir
     end
   end
 
   def test_clean_list
     restore_env do
-      ENV['OUTPUT_DIR'] = nil
+      ENV[ConcordionEnvironment.output_dir_key] = nil
       assert_equal "./*_test_output.html", ConcordionEnvironment.clean_list
-      ENV['OUTPUT_DIR'] = "foo/bar"
+      ENV[ConcordionEnvironment.output_dir_key] = "foo/bar"
       assert_equal "foo/bar/*_test_output.html", ConcordionEnvironment.clean_list
     end
 
   end
 
   def restore_env
-    orig = ENV['OUTPUT_DIR']
+    orig = ENV[ConcordionEnvironment.output_dir_key]
     yield
-    ENV['OUTPUT_DIR'] = orig
+    ENV[ConcordionEnvironment.output_dir_key] = orig
   end
 end

16ed43678a196f0571fabe92b02751ab0e6b8598
b.goodspeed <b.goodspeed@5e4cd714-1f4c-0410-be64-ad5e8ed6b23e>
2009-01-27 20:02:35 +0000

    bump the version number

--- Rakefile.rb
+++ Rakefile.rb
@@ -17,7 +17,7 @@ end
 task :default => [:clean, :test] 
 
 PKG_FILES = FileList['**/*'].exclude(/_test_output\.html$/)
-PKG_VERSION = '0.8.1'                     
+PKG_VERSION = '0.8.5'                     
 
 spec = Gem::Specification.new do |s|
   s.platform = Gem::Platform::RUBY
